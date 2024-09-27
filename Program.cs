e603efefea67b30dcc723ce54df65014862b30b6
Rely Ky <relyky@hotmail.com>
2024-09-27 19:47:55 +0800

    enhance Swagger UI

--- Program.cs
+++ Program.cs
@@ -1,20 +1,51 @@
-using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
 using N8SwagRinLab.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.AddRinLogger(); // for Rin ��ť HTTP �ʥ]
+builder.Logging.AddRinLogger(); // for Rin 監聽 HTTP 封包
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+  // 定義 Authorization 輪入欄位
+  options.AddSecurityDefinition("Bearer",
+    new OpenApiSecurityScheme
+    {
+      Name = "Authorization",
+      Type = SecuritySchemeType.ApiKey,
+      Scheme = "Bearer",
+      BearerFormat = "JWT",
+      In = ParameterLocation.Header,
+      Description = "JWT Authorization"
+    });
 
-builder.Services.AddRin(); // for Rin ��ť HTTP �ʥ]
+  // 把定義好的輪入欄位加到 UI 
+  options.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+    {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
+        {
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
+        }
+      },
+      new string[] {}
+    }
+  });
+});
 
-//## for ���d���A�ˬd
+
+builder.Services.AddRin(); // for Rin 監聽 HTTP 封包
+
+//## for 健康狀態檢查
 builder.Services.AddHealthChecks()
        .AddCheck<SimpleHealthCheck>(nameof(SimpleHealthCheck));
 
@@ -23,10 +54,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseRin(); // for Rin ��ť HTTP �ʥ]
+  app.UseRin(); // for Rin 監聽 HTTP 封包
   app.UseSwagger();
   app.UseSwaggerUI();
-  app.UseRinDiagnosticsHandler(); // for Rin ��ť HTTP �ʥ]
+  app.UseRinDiagnosticsHandler(); // for Rin 監聽 HTTP 封包
 }
 
 app.UseHttpsRedirection();
